751d71d8ac75bf71b49c76dde455499b40268283
angelsoempty <denatamanov1@gmail.com>
2023-05-23 19:19:56 +0300

    логування обчислення факторіалу

--- main.py
+++ main.py
@@ -8,3 +8,18 @@ logging.info('info')
 logging.error('error')
 logging.warning('warning')
 logging.critical('critical!!!')
+try:
+    print(10/0)
+except Exception:
+    logging.exception('Помилка')
+#факторіал числа
+
+def factorial(n):
+    logging.info(f'Розпочато обчислення факторіалу числа {n}')
+    result = 1
+    for i in range(1, n + 1):
+        result *= i #1*2*3...
+    logging.info(f'Обчислення факторіалу числа завершено {n}.Результат {result}')
+    return result
+logging.basicConfig(level=logging.INFO)
+factorial(5)
