452156a318442031b40c41fad903e789f76fdc5e
angelsoempty <denatamanov1@gmail.com>
2023-05-23 19:53:28 +0300

    2 завдання

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
 import logging
-# 1
-class Calc:
-    def __init__(self):
-        self.logger = logging.getLogger('Calculator')
-        self.logger.setLevel(logging.INFO)
-        self.logger.addHandler(logging.StreamHandler())
-    def add(self, a, b):
-        result = a + b
-        self.logger.info(f'{a} + {b} = {result}')
-        return result
-    def sub(self, a, b):
-        result = a - b
-        self.logger.info(f'{a} - {b} = {result}')
-        return result
-    def multiply(self, a, b):
-        result = a * b
-        self.logger.info(f'{a} * {b} = {result}')
-        return result
-    def divide(self, a, b):
-        if b != 0:
-            result = a / b
-            self.logger.info(f'{a} / {b} = {result}')
-            return result
-        else:
-            self.logger.error('Помилка! Ділити на нуль неможливо')
-            return None
 
-c = Calc()
-c.add(2, 2)
-c.sub(5, 1)
-c.multiply(5, 5)
-c.divide(4, 2)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+def function(filename):
+    try:
+        data = input("Введіть дані для запису: ")
+        with open(filename, 'w') as file:
+            file.write(data)
+        logger.info("Дані успішно записано до файлу.")
+    except Exception as e:
+        logger.error(f"Помилка при записі даних до файлу: {e}")
+
+filename = input("Введіть ім'я файлу: ")
+function(filename)
