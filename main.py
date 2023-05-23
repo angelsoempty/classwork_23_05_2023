38b85d44e6ab54ec1cdd005935a98569c626c07f
angelsoempty <denatamanov1@gmail.com>
2023-05-23 19:57:38 +0300

    3 завдання

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import logging
+import random
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-def function(filename):
+def function(filename, num):
     try:
-        data = input("Введіть дані для запису: ")
         with open(filename, 'w') as file:
-            file.write(data)
-        logger.info("Дані успішно записано до файлу.")
+            for i in range(num):
+                number = random.randint(1, 100)
+                file.write(str(number) + '\n')
+                logger.info(f"Згенеровано та записано число: {number}")
+        logger.info("Усі числа успішно записано до файлу.")
     except Exception as e:
-        logger.error(f"Помилка при записі даних до файлу: {e}")
+        logger.error(f"Помилка при записі чисел до файлу: {e}")
+
 
 filename = input("Введіть ім'я файлу: ")
-function(filename)
+num_numbers = int(input("Введіть кількість чисел для генерації: "))
+function(filename, num_numbers)
